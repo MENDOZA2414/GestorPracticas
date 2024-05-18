108f84eb8e075cac84c133feb119ebf897989812
MENDOZA2414 <jmendoza_21@alu.uabcs.mx>
2024-05-17 18:36:20 -0700

    base de datos y crear empresa

--- back/index.js
+++ back/index.js
@@ -1,13 +1,44 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { get } = require('http');
+const mysql = require('mysql');
 
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Jm241410',
+    database: 'vacantes_react',
+});
+
+app.use(express.json());
 app.use(cors());
 app.listen(3001, () => {
     console.log('listening on 3001')
 })
 
-app.get('/', (req,res) => {
-    res.send({status: 200});
+//empresa
+app.post('/company',(req,res)=>{
+    const company = req.body.company
+    const username = req.body.username
+    const email = req.body.email
+    const password = req.body.password
+    const logo = req.body.logo
+    db.query(`INSERT INTO company (company,username,email,password,logo) VALUES(?,?,?,md5(?),?)`,[company,username,email,password,logo],
+    (err, result) => {
+        if (err) {
+            res.send({
+                status: 400,
+                message: err
+            })
+        }else{
+            res.status(201)
+            .send({
+                status: 201,
+                message: 'Empresa creada con éxito',
+                data: result
+            })
+        }
+    }
+    );
+
 })

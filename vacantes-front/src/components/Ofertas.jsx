ee787cd0509e4e641e82f34382d41c4149131756
Bran0w0 <jesusbranc_20@alu.uabcs.mx>
2024-05-19 13:48:56 -0700

    Revert "Revert "Merge branch 'front'""
    This reverts commit f838eaa0711638ffe7396935d0f043eb03ea8aba.

---
+++ vacantes-front/src/components/Ofertas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Titulo from './common/Titulo'
+
+const Ofertas = () => {
+  return (
+    <>
+    <Titulo titulo={"Ofertas Disponibles"}/>
+    </>
+  )
+}
+
+export default Ofertas

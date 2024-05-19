3a4460c5a09051baaa75c75a099b0c10fcdc72dc
carloscampa21 <ccamapa_21@alu.uabcs.mx>
2024-05-18 23:27:21 -0700

    formulario y guardar datos

--- vacantes-front/src/components/Register.jsx
+++ vacantes-front/src/components/Register.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
-import Titulo from './common/Titulo'
+import React, { useState } from "react";
+import Titulo from "./common/Titulo";
+import axios from 'axios';
+import Swal from 'sweetalert2'
 
 export const Register = () => {
+
+  const [logo, setLogo] = useState()
+  const [company, setCompany] = useState('')
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [loading, setLoading] = useState(false)
+
+
+  const prevLogo = (e) => {
+    e.preventDefault();
+    let lector = new FileReader()
+    lector.readAsDataURL(e.target.files[0])
+    lector.onload = () => {
+      document.getElementById('logo').src = lector.result
+      setLogo(lector.result)
+    }
+  }
+
+  const registro = async(e) => {
+    e.preventDefault()
+    setLoading(true)
+    try{
+      const { data } = await axios.post(
+        `comapany`,
+        {
+          logo,
+          company,
+          username,
+          email,
+          password,
+        }
+      )
+      Swal.fire({
+        position: "top-end",
+        icon: 'success',
+        title: data.message,
+        showConfirmButton: false,
+        timer: 1500
+      })
+    }catch(err){
+      Swal.fire({
+        position: "top-end",
+        icon: 'error',
+        title: err.message,
+        showConfirmButton: false,
+        timer: 1500
+      })
+    }
+  }
   return (
     <>
-      <Titulo titulo={"Registro de empresas"} />
-      <form>
+      <Titulo titulo={"Registro de emp resas"} />
+      <form onSubmit={registro}>
         <div className="container">
           <div className="row">
             <div className="col-md">
@@ -18,6 +71,18 @@ export const Register = () => {
               <div className="card border mb-3">
                 <div className="card-body">
                   <h5 className="card-title text-center">Ingrese los datos</h5>
+                  <div className="mb-3 text-center" ><img id="logo" width='150px' src="./../../public/vite.svg" alt="" /></div>
+                  <div className="mb-3">
+                    <label className="form-label">
+                      Logo de la empresa
+                    </label>
+                    <input
+                      type="file"
+                      className="form-control"
+                      aria-describedby="emailHelp"
+                      onChange={prevLogo}
+                    />
+                  </div>
                   <div className="mb-3">
                     <label className="form-label">
                       Nombre de la empresa
@@ -26,6 +91,7 @@ export const Register = () => {
                       type="text"
                       className="form-control"
                       aria-describedby="emailHelp"
+                      onChange={(e)=> setCompany(e.target.value) }
                     />
                   </div>
                   <div className="mb-3">
@@ -36,6 +102,7 @@ export const Register = () => {
                       type="text"
                       className="form-control"
                       aria-describedby="emailHelp"
+                      onChange={(e)=> setUsername(e.target.value) }
                     />
                   </div>
                   <div className="mb-3">
@@ -46,6 +113,7 @@ export const Register = () => {
                       type="email"
                       className="form-control"
                       aria-describedby="emailHelp"
+                      onChange={(e)=> setEmail(e.target.value) }
                     />
                   </div>
                   <div className="mb-3">
@@ -56,6 +124,7 @@ export const Register = () => {
                       type="password"
                       className="form-control"
                       aria-describedby="emailHelp"
+                      onChange={(e)=> setPassword(e.target.value) }
                     />
                   </div>
                   <div className="mb-3">
@@ -66,10 +135,11 @@ export const Register = () => {
                       type="password"
                       className="form-control"
                       aria-describedby="emailHelp"
+                      onChange={(e)=> setPasswordConfirm(e.target.value) }
                     />
                   </div>
                   <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-                    <button className="btn btn-success me-md-2" type="button">Crear cuenta empresa</button>
+                    <button className="btn btn-success me-md-2" type="submit">Crear cuenta empresa</button>
                     <button className="btn btn-primary" type="button">Cancelar</button>
                   </div>
                 </div>

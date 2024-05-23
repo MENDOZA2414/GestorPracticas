de064ca544030acaedafcd0de9ab63609f83451d
MENDOZA2414 <jmendoza_21@alu.uabcs.mx>
2024-05-22 19:13:02 -0700

    Solucion registro 2

--- back/index.js
+++ back/index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.listen(PORT, () => {
-    console.log('listening on 3001');
+    console.log(`Listening on port ${PORT}`);
 });
 
 app.post('/company', upload.single('logo'), (req, res) => {
@@ -41,67 +41,69 @@ app.post('/company', upload.single('logo'), (req, res) => {
     db.query(`INSERT INTO company (company, username, email, password, logo) VALUES(?, ?, ?, md5(?), ?)`, [company, username, email, password, logo],
         (err, result) => {
             if (err) {
-                res.send({
+                res.status(400).send({
                     status: 400,
-                    message: err
+                    message: err.message
                 });
             } else {
                 res.status(201).send({
                     status: 201,
                     message: 'Empresa creada con éxito',
-                    data: result
+                    data: { insertId: result.insertId }
                 });
             }
         }
     );
 });
 
-
-app.get('/company/:id',(req,res)=>{
-    const companyId = req.params.id
-    db.query(`SELECT  company_id,company,username,email,logo FROM company WHERE company_id=${companyId}`,
-    (err, result) => {
-        if (result.length >0) {
-            res.status(200)
-            .send(result[0])
-        }else{
-            res.status(400).send({
-                message: 'No existe la empresa'
-            })
-        }
-    }
-    );
-
-})
-
-app.post('/login',(req,res)=>{
-    const email = req.body.email
-    const password = req.body.password
-    db.query(`SELECT company_id,company,username,email,logo FROM company WHERE email=? AND password=md5(?)`,[email,password],
-    (err, result) => {
-        if (err) {
-            res.send({
-                status: 500,
-                message: err
-            })
-        }else{
-            if (result.length >0) {
-                res.status(200)
-                .send(result[0])
-            }else{
-                res.status(401).send({
-                    status: 401,
-                    message: 'Usuario o contraseña incorrectos'
-                })
+app.get('/company/:id', (req, res) => {
+    const companyId = req.params.id;
+    db.query(`SELECT company_id, company, username, email, logo FROM company WHERE company_id=${companyId}`,
+        (err, result) => {
+            if (result.length > 0) {
+                const company = result[0];
+                if (company.logo) {
+                    company.logo = company.logo.toString('base64');
+                }
+                res.status(200).send(company);
+            } else {
+                res.status(400).send({
+                    message: 'No existe la empresa'
+                });
             }
-            
         }
-    }
     );
+});
 
-})
+app.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    db.query(`SELECT company_id, company, username, email, logo FROM company WHERE email=? AND password=md5(?)`, [email, password],
+        (err, result) => {
+            if (err) {
+                res.send({
+                    status: 500,
+                    message: err
+                });
+            } else {
+                if (result.length > 0) {
+                    const user = result[0];
+                    if (user.logo) {
+                        user.logo = user.logo.toString('base64');
+                    }
+                    res.status(200).send(user);
+                } else {
+                    res.status(401).send({
+                        status: 401,
+                        message: 'Usuario o contraseña incorrectos'
+                    });
+                }
+            }
+        }
+    );
+});
 
-//vacantes
+// Vacantes
 app.post('/job', (req, res) => {
     const { title, from_date, until_date, city, job_type, experience, company_id } = req.body;
     db.query(`INSERT INTO job (title, from_date, until_date, city, job_type, experience, company_id) VALUES(?,?,?,?,?,?,?)`,
@@ -129,23 +131,20 @@ app.post('/job', (req, res) => {
         });
 });
 
-
-app.get('/job/:id',(req,res)=>{
-    const id = req.params.id
-    db.query(`SELECT  * FROM job WHERE job_id=${id}`,
-    (err, result) => {
-        if (result.length >0) {
-            res.status(200)
-            .send(result[0])
-        }else{
-            res.status(400).send({
-                message: 'No existe la vacante'
-            })
+app.get('/job/:id', (req, res) => {
+    const id = req.params.id;
+    db.query(`SELECT * FROM job WHERE job_id=${id}`,
+        (err, result) => {
+            if (result.length > 0) {
+                res.status(200).send(result[0]);
+            } else {
+                res.status(400).send({
+                    message: 'No existe la vacante'
+                });
+            }
         }
-    }
     );
-
-})
+});
 
 app.get('/job/all/:company_id/:page/:limit', (req, res) => {
     const id = req.params.company_id;
@@ -164,124 +163,114 @@ app.get('/job/all/:company_id/:page/:limit', (req, res) => {
     });
 });
 
+app.get('/job/all/:page/:limit', (req, res) => {
+    const page = req.params.page;
+    const limit = req.params.limit;
+    const start = (page - 1) * limit;
 
-app.get('/job/all/:page/:limit',(req,res)=>{
-    const page = req.params.page
-    const limit = req.params.limit
-    const start = (page - 1) * limit 
-
-    db.query(`SELECT DATEDIFF(J.until_date,(select now())) as dias, J.*,C.company,C.logo FROM job J INNER JOIN company C ON C.company_id = J.company_id where deleted=0 order by job_id DESC limit ${start}, ${limit} `,
-    (err, result) => {
-        if (result.length >0) {
-            res.status(200)
-            .send(result)
-        }else{
-            res.status(400).send({
-                message: 'No existe datos'
-            })
-        }
-    }
-    );
-
-})
-
-app.put('/job/:id',(req,res)=>{
-    const id = Number(req.params.id)
-    const title = req.body.title
-    const from_date = req.body.from_date
-    const until_date = req.body.until_date
-    const city = req.body.city
-    const job_type = req.body.job_type
-    const experience = req.body.experience
-    const company_id = Number(req.body.company_id)
-
-            db.query(`UPDATE job SET title=?,from_date=?,until_date=?,city=?,job_type=?,experience=? where job_id=? and company_id=?`,[title,from_date,until_date,city,job_type,experience,id,company_id],
-            (err, result) => {
-                if (err) {
-                    res.status(400).send({
-                        message: err
-                    })
-                }else{
-                    res.status(200)
-                    .send({
-                        message: 'Vacante actualizada con éxito',
-                        data: result
-                    })
-                }
-            }
-            );
-
-    
-
-})
-app.delete('/job/:id',(req,res)=>{
-    const id = Number(req.params.id)
-    const company_id = Number(req.body.company_id)
-
-            db.query(`UPDATE job SET deleted=1 where job_id=? and company_id=?`,[id,company_id],
-            (err, result) => {
-                if (err) {
-                    res.status(400).send({
-                        message: err.message
-                    })
-                }else{
-                    res.status(200)
-                    .send({
-                        message: 'Vacante borrada con éxito',
-                        data: result
-                    })
-                }
-            }
-            );
-           
-
-
-})
-
-//personas
-app.post('/persons',(req,res)=>{
-    const dni = req.body.dni
-    const name = req.body.name
-    const email = req.body.email
-    const img = req.body.img
-
-    //consultar existencia
-    db.query(`SELECT * FROM persons WHERE dni=?`,[dni],
-    (err, result) => {
-        if (err) {
-            res.send({
-                status: 500,
-                message: err
-            })
-        }else{
-            if (result.length >0) {
-                res.status(200)
-                .send(result[0])
-            }else{
-                db.query(`INSERT INTO persons (dni,name,email,img) VALUES(?,?,?,?)`,[dni,name,email,img],
-                (err, result) => {
-                    if (err) {
-                        res.status(400).send({
-                            message: err
-                        })
-                    }else{
-                        res.status(201)
-                        .send({
-                            status: 201,
-                            message: 'Persona creada con éxito',
-                            data: result
-                        })
+    db.query(`SELECT DATEDIFF(J.until_date, (select now())) as dias, J.*, C.company, C.logo FROM job J INNER JOIN company C ON C.company_id = J.company_id where deleted=0 order by job_id DESC limit ${start}, ${limit}`,
+        (err, result) => {
+            if (err) {
+                res.status(500).send({
+                    message: err.message
+                });
+            } else {
+                result.forEach(job => {
+                    if (job.logo) {
+                        job.logo = job.logo.toString('base64');
                     }
-                })
+                });
+                res.status(200).send(result);
             }
-            
         }
-    }
-
-
     );
+});
 
-})
+app.put('/job/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const title = req.body.title;
+    const from_date = req.body.from_date;
+    const until_date = req.body.until_date;
+    const city = req.body.city;
+    const job_type = req.body.job_type;
+    const experience = req.body.experience;
+    const company_id = Number(req.body.company_id);
+
+    db.query(`UPDATE job SET title=?, from_date=?, until_date=?, city=?, job_type=?, experience=? where job_id=? and company_id=?`,
+        [title, from_date, until_date, city, job_type, experience, id, company_id],
+        (err, result) => {
+            if (err) {
+                res.status(400).send({
+                    message: err
+                });
+            } else {
+                res.status(200).send({
+                    message: 'Vacante actualizada con éxito',
+                    data: result
+                });
+            }
+        }
+    );
+});
+
+app.delete('/job/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const company_id = Number(req.body.company_id);
+
+    db.query(`UPDATE job SET deleted=1 where job_id=? and company_id=?`, [id, company_id],
+        (err, result) => {
+            if (err) {
+                res.status(400).send({
+                    message: err.message
+                });
+            } else {
+                res.status(200).send({
+                    message: 'Vacante borrada con éxito',
+                    data: result
+                });
+            }
+        }
+    );
+});
+
+// Personas
+app.post('/persons', (req, res) => {
+    const dni = req.body.dni;
+    const name = req.body.name;
+    const email = req.body.email;
+    const img = req.body.img;
+
+    // Consultar existencia
+    db.query(`SELECT * FROM persons WHERE dni=?`, [dni],
+        (err, result) => {
+            if (err) {
+                res.send({
+                    status: 500,
+                    message: err
+                });
+            } else {
+                if (result.length > 0) {
+                    res.status(200).send(result[0]);
+                } else {
+                    db.query(`INSERT INTO persons (dni, name, email, img) VALUES(?,?,?,?)`, [dni, name, email, img],
+                        (err, result) => {
+                            if (err) {
+                                res.status(400).send({
+                                    message: err
+                                });
+                            } else {
+                                res.status(201).send({
+                                    status: 201,
+                                    message: 'Persona creada con éxito',
+                                    data: result
+                                });
+                            }
+                        });
+                }
+            }
+        }
+    );
+});
 
 //vacantes
 app.post('/apply',(req,res)=>{
--- vacantes-front/src/components/Register.jsx
+++ vacantes-front/src/components/Register.jsx
@@ -1,208 +1,189 @@
 import React, { useState } from "react";
 import Titulo from "./common/Titulo";
 import axios from 'axios';
-import Swal from 'sweetalert2'
-import Error from './common/Error'
-import md5 from 'md5'
-import{Navigate} from 'react-router-dom'
+import Swal from 'sweetalert2';
+import Error from './common/Error';
+import md5 from 'md5';
+import { Navigate } from 'react-router-dom';
+import imageCompression from 'browser-image-compression';
 
 const Register = () => {
+    const [logo, setLogo] = useState();
+    const [company, setCompany] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [goLogin, setGoLogin] = useState(false);
 
-  const [logo,setLogo] = useState()
-  const [company,setCompany] = useState('')
-  const [username,setUsername] = useState('')
-  const [email,setEmail] = useState('')
-  const [password,setPassword] = useState('')
-  const [passwordConfirm,setPasswordConfirm] = useState('')
-  const [loading,setLoading] = useState(false)
-  const [error,setError] = useState(false)
-  const [goLogin,setGoLogin] = useState(false)
+    const prevLogo = async (e) => {
+        e.preventDefault();
 
+        // Compresión de imagen
+        const file = e.target.files[0];
+        const options = {
+            maxSizeMB: 1,
+            maxWidthOrHeight: 500,
+            useWebWorker: true
+        };
 
-  const prevLogo = (e) => {
-    e.preventDefault();
-    let lector = new FileReader()
-    lector.readAsDataURL(e.target.files[0])
-    lector.onload = () => {
-      document.getElementById('logo').src = lector.result
-      setLogo(lector.result)
-    }
-  }
-
-  const limpiarCampos = () => {
-    setCompany('')
-    setEmail('')
-    setUsername('')
-    setPassword('')
-    setPasswordConfirm('')
-    setLogo('')
-  }
-
-  const registro = async (e) => {
-    e.preventDefault()
-
-    let dataCom = {logo,company,username,email}
-    //validar 
-    if([logo,company,username,email,password,passwordConfirm].includes('') || [logo,company,username,email,passwordConfirm].includes('#')){
-      setError(true)
-      Swal.fire({
-        position: 'center',
-        icon: 'error',
-        title: "Debe llenar todos los campos",
-        showConfirmButton: false,
-        timer: 1500
-      })
-      return
-    }else if(password!==passwordConfirm){
-      setPassword('')
-      setPasswordConfirm('')
-      Swal.fire({
-        position: 'center',
-        icon: 'warning',
-        title: "Las contraseñas no coinciden",
-        showConfirmButton: false,
-        timer: 1500
-      })
-      setError(true)
-      return
-    }else setError(false)
-
-    setLoading(true)
-    try{
-      const { data } = await axios.post(
-        `company`,
-        {
-          logo,
-          company,
-          username,
-          email,
-          password
+        try {
+            const compressedFile = await imageCompression(file, options);
+            const lector = new FileReader();
+            lector.readAsDataURL(compressedFile);
+            lector.onload = () => {
+                document.getElementById('logo').src = lector.result;
+                setLogo(compressedFile);
+            };
+        } catch (error) {
+            console.error('Error al comprimir la imagen:', error);
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: "Error al comprimir la imagen",
+                showConfirmButton: false,
+                timer: 1500
+            });
         }
-      )
-      Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: data.message,
-        showConfirmButton: false,
-        timer: 1500
-      })
-      dataCom.id = await data.data.insertId
-      const idSession = await md5(dataCom.id+dataCom.email+dataCom.username)
-      localStorage.setItem('user',JSON.stringify(dataCom))
-      localStorage.setItem('idSession',idSession)
-      setGoLogin(true)
-    }catch(err){
-      Swal.fire({
-        position: 'top-end',
-        icon: 'error',
-        title: err.message,
-        showConfirmButton: false,
-        timer: 1500
-      })
+    };
+
+    const limpiarCampos = () => {
+        setCompany('');
+        setEmail('');
+        setUsername('');
+        setPassword('');
+        setPasswordConfirm('');
+        setLogo('');
+    };
+
+    const registro = async (e) => {
+        e.preventDefault();
+
+        if ([logo, company, username, email, password, passwordConfirm].includes('') || [logo, company, username, email, passwordConfirm].includes('#')) {
+            setError(true);
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: "Debe llenar todos los campos",
+                showConfirmButton: false,
+                timer: 1500
+            });
+            return;
+        } else if (password !== passwordConfirm) {
+            setPassword('');
+            setPasswordConfirm('');
+            Swal.fire({
+                position: 'center',
+                icon: 'warning',
+                title: "Las contraseñas no coinciden",
+                showConfirmButton: false,
+                timer: 1500
+            });
+            setError(true);
+            return;
+        } else setError(false);
+
+        setLoading(true);
+        try {
+            const formData = new FormData();
+            formData.append('logo', logo);
+            formData.append('company', company);
+            formData.append('username', username);
+            formData.append('email', email);
+            formData.append('password', password);
+
+            const { data } = await axios.post(`http://localhost:3001/company`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: data.message,
+                showConfirmButton: false,
+                timer: 1500
+            });
+
+            const dataCom = { id: data.data.insertId, logo: URL.createObjectURL(logo), company, username, email };
+            const idSession = md5(dataCom.id + dataCom.email + dataCom.username);
+            localStorage.setItem('user', JSON.stringify(dataCom));
+            localStorage.setItem('idSession', idSession);
+            setGoLogin(true);
+        } catch (err) {
+            Swal.fire({
+                position: 'top-end',
+                icon: 'error',
+                title: err.message,
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }
+        limpiarCampos();
+        setLoading(false);
+    };
+
+    if (goLogin) {
+        return <Navigate to="/misOfertas" />;
     }
-    limpiarCampos()
 
-  }
-
-  if(goLogin){
-    return <Navigate to="/misOfertas" />
-  }
-
-  return (
-    <>
-      <Titulo titulo="Registro de empresas" />
-      <form onSubmit={registro}>
-        <div className="container">
-          <div className="row">
-            
-            <div className="col-md">
-              <img width="100%" src="./../../public/slider/slide1.jpg" alt="" />
-              <p>Accede a nuestra comunidad de talento y haz un seguimiento de tus candidaturas</p>
-              <p>Aplica a todas nuestras ofertas sin tener que repetir tu información una y otra vez</p>
-              <p>Mantente informado de nuevas ofertas que sean de tu interés</p>
-            </div>
-            <div className="col-md">
-              <div className="card border mb-3">
-                <div className="card-body">
-                  <h5 className="card-title text-center">Ingrese los datos</h5>
-                  
-                  <div className="mb-3 text-center"><img id="logo" width='150px' src="./../../public/vite.svg" alt="" /></div>
-                  <div className="mb-3">
-                    <label className="form-label">Logo de la empresa</label>
-                    <input
-                      type="file"
-                      className="form-control"
-                      aria-describedby="emailHelp"
-                      onChange={prevLogo}
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <label className="form-label">Nombre de la empresa</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      aria-describedby="emailHelp"
-                      onChange={(e) => setCompany(e.target.value)}
-                      value={company}
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <label className="form-label">Nombre del usuario</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      aria-describedby="emailHelp"
-                      onChange={(e) => setUsername(e.target.value)}
-                      value={username}
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <label className="form-label">Email</label>
-                    <input
-                      type="email"
-                      className="form-control"
-                      aria-describedby="emailHelp"
-                      onChange={(e) => setEmail(e.target.value)}
-                      value={email}
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <label className="form-label">Contraseña</label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      aria-describedby="emailHelp"
-                      onChange={(e) => setPassword(e.target.value)}
-                      value={password}
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <label className="form-label">Confirmar Contraseña</label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      aria-describedby="emailHelp"
-                      onChange={(e) => setPasswordConfirm(e.target.value)}
-                      value={passwordConfirm}
-                    />
-                  </div>
-                  <div className="d-grid gap-2 d-md-flex justify-content-md-end mb-3">
-                    <button className="btn btn-success me-md-2" type="submit">
-                      Crear cuenta empresa
-                    </button>
-                    <button onClick={limpiarCampos} className="btn btn-primary" type="button">
-                      Cancelar
-                    </button>
-                  </div>
-                  {error && <Error mensaje='Todos los campos son obligatorios' />}
+    return (
+        <>
+            <Titulo titulo="Registro de empresas" />
+            <form onSubmit={registro}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md">
+                            <img width="100%" src="./../../public/slider/slide1.jpg" alt="" />
+                            <p>Accede a nuestra comunidad de talento y haz un seguimiento de tus candidaturas</p>
+                            <p>Aplica a todas nuestras ofertas sin tener que repetir tu información una y otra vez</p>
+                            <p>Mantente informado de nuevas ofertas que sean de tu interés</p>
+                        </div>
+                        <div className="col-md">
+                            <div className="card border mb-3">
+                                <div className="card-body">
+                                    <h5 className="card-title text-center">Ingrese los datos</h5>
+                                    <div className="mb-3 text-center"><img id="logo" width='150px' src="./../../public/vite.svg" alt="" /></div>
+                                    <div className="mb-3">
+                                        <label className="form-label">Logo de la empresa</label>
+                                        <input type="file" className="form-control" onChange={prevLogo} />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label className="form-label">Nombre de la empresa</label>
+                                        <input type="text" className="form-control" onChange={(e) => setCompany(e.target.value)} value={company} />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label className="form-label">Nombre del usuario</label>
+                                        <input type="text" className="form-control" onChange={(e) => setUsername(e.target.value)} value={username} />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label className="form-label">Email</label>
+                                        <input type="email" className="form-control" onChange={(e) => setEmail(e.target.value)} value={email} />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label className="form-label">Contraseña</label>
+                                        <input type="password" className="form-control" onChange={(e) => setPassword(e.target.value)} value={password} />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label className="form-label">Confirmar Contraseña</label>
+                                        <input type="password" className="form-control" onChange={(e) => setPasswordConfirm(e.target.value)} value={passwordConfirm} />
+                                    </div>
+                                    <div className="d-grid gap-2 d-md-flex justify-content-md-end mb-3">
+                                        <button className="btn btn-success me-md-2" type="submit">Crear cuenta empresa</button>
+                                        <button onClick={limpiarCampos} className="btn btn-primary" type="button">Cancelar</button>
+                                    </div>
+                                    {error && <Error mensaje='Todos los campos son obligatorios' />}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </form>
-    </>
-  );
+            </form>
+        </>
+    );
 };
 
 export default Register;

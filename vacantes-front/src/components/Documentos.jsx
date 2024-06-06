27a4b8b557ad62c49db62a01844b964f14761bff
Bran0w0 <jesusbranc_20@alu.uabcs.mx>
2024-06-06 16:47:27 -0700

    Cambio componente documentos

--- vacantes-front/src/components/Documentos.jsx
+++ vacantes-front/src/components/Documentos.jsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { FaFilePdf, FaEye, FaDownload, FaPaperPlane, FaTrash } from 'react-icons/fa';
+import { FaFilePdf, FaEye, FaDownload, FaPaperPlane, FaTrash, FaFolder } from 'react-icons/fa';
 import Swal from 'sweetalert2';
 import './documentos.css';
 
 const Documentos = () => {
     const [documents, setDocuments] = useState([]);
+    const [sentDocuments, setSentDocuments] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [selectedDocument, setSelectedDocument] = useState(null);
+    const [alumno, setAlumno] = useState({});
+    const [searchTerm, setSearchTerm] = useState('');
+    const [viewSentDocuments, setViewSentDocuments] = useState(false);
 
     useEffect(() => {
         const fetchDocuments = async () => {
@@ -20,11 +24,24 @@ const Documentos = () => {
                     throw new Error('No se encontró el número de control del alumno logueado');
                 }
 
+                // Fetch documentos subidos
                 const response = await axios.get(`http://localhost:3001/documentosAlumno/${numControl}`);
                 setDocuments(response.data);
                 if (response.data.length === 0) {
                     setError('No se encontraron documentos.');
                 }
+
+                // Fetch documentos enviados
+                const sentResponse = await axios.get(`http://localhost:3001/documentosEnviados/${numControl}`);
+                setSentDocuments(sentResponse.data);
+                if (sentResponse.data.length === 0) {
+                    setError('No se encontraron documentos enviados.');
+                }
+
+                // Fetch alumno info
+                const alumnoResponse = await axios.get(`http://localhost:3001/alumno/${numControl}`);
+                setAlumno(alumnoResponse.data);
+
             } catch (error) {
                 setError('Error fetching documents');
             } finally {
@@ -160,61 +177,107 @@ const Documentos = () => {
         handleCloseModal();
     };
 
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const filteredDocuments = viewSentDocuments ? sentDocuments.filter((doc) =>
+        doc.nombreArchivo.toLowerCase().includes(searchTerm.toLowerCase())
+    ) : documents.filter((doc) =>
+        doc.nombreArchivo.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const toggleView = () => {
+        setViewSentDocuments(!viewSentDocuments);
+        setError(null); // Reset error when toggling view
+    };
+
     return (
-        <div className="card">
-            <div className="card-header">
-                <div className="upload-button">
-                    <input
-                        type="file"
-                        accept="application/pdf"
-                        id="file-upload"
-                        style={{ display: 'none' }}
-                        onChange={handleFileUpload}
-                    />
-                    <label htmlFor="file-upload" className="upload-label">
-                        <FaFilePdf className="upload-icon" />
-                        <span>Subir Documento PDF</span>
-                    </label>
-                </div>
-            </div>
-            <div className="card-body">
-                <div className="documents-list">
-                    <h3>Documentos Subidos</h3>
-                    {loading ? (
-                        <p>Cargando...</p>
-                    ) : error ? (
-                        <p>{error}</p>
-                    ) : (
-                        <ul>
-                            {documents.map((doc) => (
-                                <li key={doc.id}>
-                                    <div className="document-name">{doc.nombreArchivo}</div>
-                                    <div className="document-actions">
-                                        <FaEye className="action-icon" title="Ver" onClick={() => handleView(doc.id)} />
-                                        <FaDownload className="action-icon" title="Descargar" onClick={() => handleDownload(doc.id, doc.nombreArchivo)} />
-                                        <FaPaperPlane className="action-icon" title="Enviar" onClick={() => handleSend(doc)} />
-                                        <h5 className='barrita'>|</h5>
-                                        <FaTrash className="action-icon trash-icon" title="Eliminar" onClick={() => handleDelete(doc.id)} />
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                </div>
-            </div>
-            {selectedDocument && (
-                <div className="modal">
-                    <div className="modal-content">
-                        <span className="close-button" onClick={handleCloseModal}>&times;</span>
-                        <div className="modal-body">
-                            <FaFilePdf className="modal-icon" />
-                            <p>¿Deseas enviar este PDF para continuar con tu práctica profesional?</p>
-                            <p>{selectedDocument.nombreArchivo}</p>
-                            <button className="confirm-button" onClick={handleConfirmSend}>Enviar a asesor interno</button>
-                        </div>
+        <div className="documentos-container">
+            <div className="alumno-card">
+                <img src={alumno.foto} alt="Foto del alumno" className="alumno-foto" />
+                <div className="alumno-info">
+                    <h4>{alumno.nombre}</h4>
+                    <div className="alumno-details">
+                        <p>Carrera: {alumno.carrera}</p>
+                        <p>Turno: {alumno.turno}</p>
                     </div>
                 </div>
-            )}
+                <div className="alumno-folder-icon" onClick={toggleView}>
+                    <div className="folder-circle">
+                        <FaFolder />
+                    </div>
+                </div>
+            </div>
+            <div className="card">
+                <div className="card-header">
+                    {!viewSentDocuments && (
+                        <div className="upload-button">
+                            <input
+                                type="file"
+                                accept="application/pdf"
+                                id="file-upload"
+                                style={{ display: 'none' }}
+                                onChange={handleFileUpload}
+                            />
+                            <label htmlFor="file-upload" className="upload-label">
+                                <FaFilePdf className="upload-icon" />
+                                <span>Subir Documento PDF</span>
+                            </label>
+                        </div>
+                    )}
+                </div>
+                <div className="card-body">
+                    <div className="search-bar2">
+                        <input
+                            type="text"
+                            placeholder="Buscar PDF..."
+                            value={searchTerm}
+                            onChange={handleSearch}
+                        />
+                    </div>
+                    <div className="documents-list">
+                        <h3>{viewSentDocuments ? 'Documentos Enviados' : 'Documentos Subidos'}</h3>
+                        {loading ? (
+                            <p>Cargando...</p>
+                        ) : error ? (
+                            <p>{error}</p>
+                        ) : (
+                            <ul>
+                                {filteredDocuments.map((doc) => (
+                                    <li key={doc.id}>
+                                        <div className="document-name">{doc.nombreArchivo}</div>
+                                        <div className="document-actions">
+                                            <FaEye className="action-icon" title="Ver" onClick={() => handleView(doc.id)} />
+                                            <FaDownload className="action-icon" title="Descargar" onClick={() => handleDownload(doc.id, doc.nombreArchivo)} />
+                                            {!viewSentDocuments && (
+                                                <>
+                                                    <FaPaperPlane className="action-icon" title="Enviar" onClick={() => handleSend(doc)} />
+                                                    <h5 className='barrita'>|</h5>
+                                                    <FaTrash className="action-icon trash-icon" title="Eliminar" onClick={() => handleDelete(doc.id)} />
+                                                </>
+                                            )}
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </div>
+                </div>
+                {selectedDocument && (
+                    <div className="modal">
+                        <div className="modal-content">
+                            <span className="close-button" onClick={handleCloseModal}>&times;</span>
+                            <div className="modal-body">
+                                <FaFilePdf className="modal-icon" />
+                                <p>¿Deseas enviar este PDF para continuar con tu práctica profesional?</p>
+                                <p>{selectedDocument.nombreArchivo}</p>
+                                <button className="confirm-button" onClick={handleConfirmSend}>Enviar a asesor interno</button>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
